c603e06ecc8f1adb31a070a3e8148050045c3a52
Ajitha-Rajkumar <Ajitha.Rajkumar@ril.com>
2025-08-04 16:54:50 +0530

    Update compound_interest.py

--- compound_interest.py
+++ compound_interest.py
@@ -2,7 +2,7 @@
 # Do not use this in production. Sample purpose only.
 
 # Author: Upkar Lidder (IBM)
-
+## these changes are added by Ajitha
 # Input:
 # p, principal amount
 # t, time period in years
